aea4021b5b37a1db12653d384345347eff7a795a
Никита Воронович <zvart1one@yandex.ru>
2024-08-27 15:31:13 +0300

    start dev calc

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Калькулятор:")
+	fmt.Println("1. Арабские числа от 1 до 10 включительно.")
+	fmt.Println("2. Римские числа от I до X включительно.")
+	fmt.Println("3. Арифметические операции +, -, *, /")
+
+}
